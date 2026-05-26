6b7f983fa8ce199ab4981dd29e7c483809eca005
djogos <38543724+djogos@users.noreply.github.com>
2026-05-26 13:40:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ from datetime import datetime
 from pymongo import MongoClient
 
 MONGO_URI = os.environ.get("MONGO_URI")
-client = MongoClient(MONGO_URI)
+client = MongoClient(
+    MONGO_URI, 
+    serverSelectionTimeoutMS=5000,
+    tls=True,
+    tlsAllowInvalidCertificates=True
+)
 db = client["nesa"]  # Ime baze podataka
 tickets_collection = db["orders"]
 
